6ff0c7e7a8beb2d7ac1af4debc14b391f430a558
jdarenas45 <darenas132@gmail.com>
2026-06-02 12:28:04 -0500

    Corrige URL del backend en produccion

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://modaclic-backend-production-35a0.up.railway.app/api",
   headers: {
     'Content-Type': 'application/json'
   }
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/auth";
+const API = "https://modaclic-backend-production-35a0.up.railway.app/auth";
 
 export const login = (data) => axios.post(`${API}/login`, data);
 export const register = (data) => axios.post(`${API}/register`, data);
